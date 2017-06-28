ce80f81394b57d9b46f8936a5cebff9620cd45e3
Mariusz Saramak <mariusz.saraamk@tomtom.com>
2017-06-28 15:10:56 +0200

    Search and get usernames

--- checkin/ViewController.swift
+++ checkin/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var userTableView: UITableView!
+    private var filterText: String?
+    @IBAction func searchChangedEdit(_ sender: UITextField){
+        filterText = sender.text
+        self.userFilteredList = self.userList.filter {$0.contains(filterText ?? "")}
+        self.userTableView.reloadData()
+    }
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    
+    var userList: [String] = []
+    var userFilteredList: [String] = []
+    var ref : DatabaseReference?
+    var handle : DatabaseHandle?
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return userFilteredList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = userFilteredList[indexPath.row]
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ref = Database.database().reference();
+        let childRef =  ref?.child("/");
+        
+        handle = childRef?.observe(.value, with: { (snapshot) in
+            print("children \(snapshot.childrenCount)")
+            let enumerator = snapshot.children
+            self.userList.removeAll()
+            while let rest = enumerator.nextObject() as? DataSnapshot {
+                guard let restDict = rest.value as? [String: Any] else { continue }
+                let name = restDict["first"] as? String
+                let last = restDict["last"] as? String
+                let email = restDict["email"] as? String
+                let itemDisplay = name?.appending(" ").appending(last!).appending(" ").appending(email!);
+                self.userList.append(itemDisplay!)
+                
+                
+            }
+            self.userFilteredList = self.userList.filter { $0 is String }
+            self.userTableView.reloadData()
+            
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  
 
 }
 
